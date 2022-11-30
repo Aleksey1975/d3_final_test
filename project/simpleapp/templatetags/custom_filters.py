c6bade13374a7b32f790e8446c112c1cf61d418e
Aleksey1975 <87759258+Aleksey1975@users.noreply.github.com>
2022-11-30 13:20:23 +0300

    Add files via upload

---
+++ project/simpleapp/templatetags/custom_filters.py
@@ -0,0 +1,38 @@
+from django import template
+
+register = template.Library()
+
+CURRENCIES_SYMBOLS = {
+    'rub': 'Р',
+    'usd': '$',
+}
+
+
+@register.filter()
+def currency(value, code='rub'):
+    """
+   value: значение, к которому нужно применить фильтр
+   code: код валюты
+   """
+    postfix = CURRENCIES_SYMBOLS[code]
+
+    return f'{value} {postfix}'
+
+
+@register.filter()
+def censor(value):
+    if type(value) is not str:
+        raise ValueError
+    obscene_words = ('редиск', 'баклажан', 'капуст')
+    punctuations = ',.!?;:-'
+    text = value.split()
+    for i in range(len(text)):
+        for check in obscene_words:
+            if check in text[i].lower():
+                if text[i][-1] in punctuations:
+                    text[i] = f'{text[i][0]}{"*" * (len(text[i]) - 2)}{text[i][-1]}'
+                else:
+                    text[i] = f'{text[i][0]}{"*"*(len(text[i])-1)}'
+    text = ' '.join(text)
+
+    return f'{text} '
---
+++ project/simpleapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+# Импортируем созданное нами представление
+from .views import *
+
+urlpatterns = [
+   # path — означает путь.
+   # В данном случае путь ко всем товарам у нас останется пустым,
+   # чуть позже станет ясно почему.
+   # Т.к. наше объявленное представление является классом,
+   # а Django ожидает функцию, нам надо представить этот класс в виде view.
+   # Для этого вызываем метод as_view.
+   path('', ProductsList.as_view()),
+   path('<int:pk>', ProductDetail.as_view()),
+   path('news/', NewsList.as_view()),
+   path('news/<int:pk>', NewsDetail.as_view()),
+   path('pages/', include('django.contrib.flatpages.urls')),
+]
